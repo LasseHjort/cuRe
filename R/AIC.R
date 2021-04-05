890b13d7b2ec93e9d64ea36220e8d992e6ccf7e2
Rasmus Kuhr Jensen <efpe@aas.its.nja.dk>
2021-04-05 11:59:17 +0200

    Finished AIC and BIC functions and their documentations

--- R/AIC.R
+++ R/AIC.R
@@ -1,9 +1,9 @@
 #' Akaike information criterion
 #'
-#' This function computes the AIC for a fitted parametric cure model
+#' This function computes the AIC for a fitted parametric cure model.
 #'
 #' @param object Object of class cm or gfcm.
-#' @param k Number to control if either AIC or BIC is to be computed (default is 2 equal to AIC)
+#' @param k Number to control if either AIC or BIC is to be computed (default is 2 equal to AIC).
 #' @export
 #' @method AIC cm
 AIC.cm <- function(object, k = 2){
@@ -14,6 +14,31 @@ AIC.cm <- function(object, k = 2){
   return(aic)
 }
 
+#' Akaike information criterion
+#'
+#' This function computes the AIC for a fitted parametric cure model.
+#'
+#' @param object Object of class cm or gfcm.
+#' @param k Number to control if either AIC or BIC is to be computed (default is 2 equal to AIC).
+#' @export
+#' @method AIC gfcm
+AIC.gfcm <- function(object, k = 2){
+  npar <- length(object$coefs) + length(object$coefs.spline)
+  lnL <- -object$NegMaxLik
+  aic <- as.vector(-2*lnL + k * npar)
+
+  return(aic)
+}
+
+
+
+
+
+#' Bayesian information criterion
+#'
+#' This function computes the BIC for a fitted parametric cure model.
+#'
+#' @param object Object of class cm or gfcm.
 #' @export
 #' @method BIC cm
 BIC.cm <- function(object, ...)
@@ -23,8 +48,32 @@ BIC.cm <- function(object, ...)
   return(bic)
 }
 
+#' Bayesian information criterion
+#'
+#' This function computes the BIC for a fitted parametric cure model.
+#'
+#' @param object Object of class cm or gfcm.
+#' @export
+#' @method BIC gfcm
+BIC.gfcm <- function(object, ...)
+{
+  bic <- AIC(object, k = log(nobs(object)))
+
+  return(bic)
+}
+
+
+
+
+
 #' @method nobs cm
 nobs.cm <- function(object, ...){
+  val <- nrow(object$data)
+  return(val)
+}
+
+#' @method nobs gfcm
+nobs.gfcm <- function(object, ...){
   val <- nrow(object$data)
   return(val)
 }
